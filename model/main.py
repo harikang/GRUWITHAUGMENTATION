0ba8da550a037596bfd39ae8b8cf49f210eef4cf
HariKang <138555992+harikang@users.noreply.github.com>
2024-06-13 16:31:36 +0900

    Update main.py

--- model/main.py
+++ model/main.py
@@ -1,9 +1,8 @@
-# main.py
-
-from train_test_split import change_hiddim
+from train_test_split import train_data_loader, test_data_loader, device
+from model import change_hiddim
 
 epochs = 500
 learning_rate = 1e-3
-
+num_classes  = 6
 model, best_acc, best_epoch, training_time = change_hiddim(300, 'gru_modified', num_classes, epochs, learning_rate)
 print(f"Best Accuracy: {best_acc:.2f} %, Epoch: {best_epoch}, Time: {training_time:.2f} seconds")
